eb616b153b2f42bbac7465cf240a2c8f300c1a09
dreamsoftech <dreamsoftech88@gmail.com>
2022-09-23 20:39:59 +0800

    Add decorators

---
+++ decorators/decorators.gemspec
@@ -0,0 +1,28 @@
+Gem::Specification.new do |s|
+  s.platform          = Gem::Platform::RUBY
+  s.name              = %q{decorators}
+  s.version           = %q{2.0.5}
+  s.description       = %q{Manages the process of loading decorators into your Rails application.}
+  s.summary           = %q{Rails decorators plugin.}
+  s.email             = %q{gems@p.arndt.io}
+  s.homepage          = %q{https://github.com/parndt/decorators}
+  s.authors           = ["Philip Arndt"]
+  s.license           = %q{MIT}
+  s.require_paths     = %w(lib)
+
+  s.files             = %w[
+    lib/decorators.rb
+    lib/decorators/paths.rb
+    lib/decorators/railtie.rb
+    license.md
+    readme.md
+  ]
+
+  s.add_dependency             "railties", ">= 4.0.0"
+  s.add_development_dependency "rspec", "~> 3.5", ">= 3.5.0"
+
+  s.cert_chain  = ["certs/parndt.pem"]
+  if $0 =~ /gem\z/ && ARGV.include?("build") && ARGV.include?(__FILE__)
+    s.signing_key = File.expand_path("~/.ssh/gem-private_key.pem")
+  end
+end
---
+++ decorators/spec/decorators_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require_relative 'support/decorate_me.rb'
+
+describe Decorators do
+  before do
+    Decorators.register! File.expand_path('../support', __FILE__)
+    Decorators.load! true
+  end
+
+  let(:decorated_class) { DecorateMe.new }
+  it 'register decorators' do
+    decorators = [
+      decorator_path('decorate_me_decorator'),
+      decorator_path('another_decorator')
+    ]
+    bad_decorators = [
+      decorator_path('../do_not_load_this_decorator')
+    ]
+    decorators.each do |decorator|
+      expect(Decorators.decorators).to include(decorator)
+    end
+    # Ensure that we don't load from app/decorators/<here>
+    # but only app/decorators/subdirectory/<here>
+    bad_decorators.each do |bad_decorator|
+      expect(Decorators.decorators).to_not include(bad_decorator)
+    end
+  end
+
+  it 'decorates class with same file name' do
+    expect(decorated_class.kiwi).to eq("parndt")
+  end
+
+  it 'decorates class with different file name' do
+    expect(decorated_class.am_i_decorated?).to eq(true)
+  end
+end
+
